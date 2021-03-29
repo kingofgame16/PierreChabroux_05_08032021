708777e4e8d499d74b9652d04d8e9860e245b70c
kingofgame16 <pierrechabroux@gmail.com>
2021-03-29 11:23:48 +0200

    modif

---
+++ frontend/panier.js
@@ -0,0 +1,5 @@
+const inHtml = document.getElementById("main");
+const prixInHtml = document.getElementById("finalPrice");
+const btnCommande = document.getElementById("btnCom");
+let data = JSON.parse(localStorage.getItem("basket"));
+
--- frontend/produit.js
+++ frontend/produit.js
@@ -1,7 +1,85 @@
-import {displayProduct} from './js/product/displayProduct.js'
+const inHtml = document.getElementById('main');
+    const params = new URLSearchParams(window.location.search);
+    
+    
+    //j'injecte l'id du produit clické dans le fetch
+    fetch(`http://localhost:3000/api/furniture/${params.get('id')}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                Promise.reject(response.status);
+            }
+        })
+        .then(data => {
+    
+            //--variable prix pour le diviser par 100
+            let priceProdUnit = data.price / 100;
+    
+            //--variable vide + boucle pour créer le select 
+            let varnished = "";
+    
+            data.varnish.forEach(verni => {
+                varnished += `<option value="${verni}">${verni}</option>`;
+            })
+    
+            //--Ecriture du HTML en dynamique
+            inHtml.innerHTML += `
+                    <div class="card card-body col-12 col-lg-6">
+                        <img alt="${data.name}" class="img-fluid" src="${data.imageUrl}">
+                    </div>
+                    <div class="card col-12 col-lg-6 pb-3">
+                        <h2>${data.name}</h2>
+                        <p>${data.description}</p>
+                        <form>
+                            <label for="quantiteProduit">Quantité:</label>
+                            <input id ="quantiteProduit" type="number" min="1" value="1"/>
+                                <div class="col-auto my-1 pb-5 mt-4">
+                                    <label class="mr-sm-2" for="inlineFormCustomSelect">Vernis</label>
+                                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                        ${varnished}   
+                                    </select>        
+                                </div>
+                            <p><strong>Prix total</strong> : <span id="totalPrice">${priceProdUnit}</span> €</p>                       
+                        </form>   
+                    </div>
+                    `;
+        })
 
-displayProduct()
+const addBasketBtnElement = document.querySelector('#btnAddBasket')
+    const monProduit = {
+        id: 1,
+        name: 'nomproduit',
+        description: 'madescription',
+        price: 1000,
+        imageUrl: 'monprouit.jpeg',
+        quantity: 1
+    }
+    addBasketBtnElement.addEventListener('click', () => {
+        alert ('Produit bien ajouté au panier')
+        addLocalStorage(monProduit)
+    })
 
-import {AddBasketProduct} from './js/product/AddBasketProduct.js'
+const addLocalStorage = (product) => {
+    let basketFull = JSON.parse(localStorage.getItem("basket"));
 
-AddBasketProduct()
+    if (!basketFull) {
+        localStorage.setItem("basket", JSON.stringify([product]))
+    } else {
+        const basket = JSON.parse(localStorage.getItem("basket"))
+        const productAlreadySelected = basket.filter(p => p.id === product.id)
+        if (productAlreadySelected.length > 0) {
+            productAlreadySelected[0].quantity++
+        } else {
+            basket.push(product)
+        }
+    }
+}
+
+function calculePrice(priceProdUnit) {
+    let quantites = document.getElementById('quantiteProduit');
+    quantites.addEventListener('change', (event) => {
+        const result = document.getElementById('totalPrice');
+        result.textContent = `${priceProdUnit}` * `${event.target.value}`;
+    })
+}
