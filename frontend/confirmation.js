8e6e9bfa4e5c9b48c2ef44b372fe443415afd245
kingofgame16 <pierrechabroux@gmail.com>
2021-05-04 08:58:01 +0200

    dernière modif

--- frontend/confirmation.js
+++ frontend/confirmation.js
@@ -1,8 +1,11 @@
+'use strict';
 const totalPriceElement = document.getElementById('totalPrice');
 const orderIdElement = document.getElementById('orderId');
 
 const totalPrice = JSON.parse(localStorage.getItem('totalPrice'));
 const orderId = JSON.parse(localStorage.getItem('orderId'));
 
-totalPriceElement.innerText = totalPrice; 
+totalPriceElement.innerText = totalPrice +'€'; 
 orderIdElement.innerText = orderId; 
+
+
